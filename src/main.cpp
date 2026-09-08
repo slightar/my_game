ab026191730b67ad3b5e7bee9f1642117168c825
slightar <3484461704@qq.com>
2026-09-08 09:18:47 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 34844 on 2026/9/8.
+//
+#include "raylib.h"
+
+int main() {
+    InitWindow(800, 600, "Dungeon Crawler");   // 创建 800x600 窗口
+    SetTargetFPS(60);                          // 限制帧率为 60
+
+    while (!WindowShouldClose()) {             // 主循环：窗口不关闭就一直运行
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, my game!", 300, 280, 24, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
